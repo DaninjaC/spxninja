a652304edd8b3926444f8d3049e2d867b91bbc46
daninjac <156266146+DaninjaC@users.noreply.github.com>
2026-07-14 23:01:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,43 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'rota-ninja-v7';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/css/estilo-base.css',
+  '/css/layout.css',
+  '/js/main.js',
+  '/js/leitor.js',
+  '/js/motor-auto.js',
+  '/js/motor-manual.js',
+  '/js/motor-doca.js',
+  '/js/motor-gps.js',
+  '/assets/ninjaspx.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  // Apenas para cumprir o requisito de instalação do PWA
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((res) => {
+      return res || fetch(evt.request);
+    })
+  );
 });
